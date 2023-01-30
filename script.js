575e6a1b90dbc876319c9e99973138460ab1810e
rishabhgoswami8 <123253910+rishabhgoswami8@users.noreply.github.com>
2023-01-30 23:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  var name = document.querySelector("#name").value;
+  var year = document.querySelector("#year").value;
+  document.querySelector("#url").textContent = "https://localhost:8080/?name=" + name + "&year=" + year;
+});
+
